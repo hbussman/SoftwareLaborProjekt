1f823a99c62d875067235002ddef394b8ac6c04f
Patrick Pitz <papitz@stud.hs-heilbronn.de>
2019-04-10 16:43:02 +0200

    generate html with all sponsors

--- src/main/java/sponsoren/service/ServerPageController.java
+++ src/main/java/sponsoren/service/ServerPageController.java
@@ -1,14 +1,28 @@
 package sponsoren.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import sponsoren.orm.SponsorEntity;
 
 @Controller
 public class ServerPageController {
 
+  @Autowired
+  private SponsorRepository sponsorRepository;
+
   @GetMapping({"/", "/index"})
   public String hello(Model model) {
+    //get all Sponsors
+    Iterable<SponsorEntity> sponsorEntities = sponsorRepository.findAll();
+    List<SponsorEntity> sponsors = new ArrayList<>();
+    //convert iterable to List
+    sponsorEntities.forEach(sponsors::add);
+
+    model.addAttribute("sponsors", sponsors);
     return "index";
   }
 
